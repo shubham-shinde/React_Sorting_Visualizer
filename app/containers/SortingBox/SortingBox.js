61669431558850f68db866029abc5d0e1f795c0f
shubham-shinde <shindes0321@gmail.com>
2019-11-28 23:57:24 +0530

    pointer and white activated working on ui

--- app/containers/SortingBox/SortingBox.js
+++ app/containers/SortingBox/SortingBox.js
@@ -11,7 +11,7 @@ class SortingBox extends React.Component {
       <div className="SortingBox">
         { 
           list.map(i=>(
-            <Piller height={i.num} white={i.white} />
+            <Piller height={i.num} white={i.white} pointer={i.pointer} />
           ))
         }
       </div>
