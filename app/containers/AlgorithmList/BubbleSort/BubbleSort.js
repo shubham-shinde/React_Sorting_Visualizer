6c66604a1181dba1e433bd8db6ca68b155550c46
shivam1534 <shivam.mkp@gmail.com>
2019-11-29 00:01:07 +0530

    selection

--- app/containers/AlgorithmList/BubbleSort/BubbleSort.js
+++ app/containers/AlgorithmList/BubbleSort/BubbleSort.js
@@ -99,24 +99,16 @@ function mergeSort (unsortedArray) {
 
 
   //Inserion Sort
-
-  const checkLoopInvariant = (newArr, originalArr, index) => {
-    //need to slice at least 1 element out
-    if (index < 1) index = 1
-  
-    newArr = newArr.slice(0,index)
-    originalArr = originalArr.slice(0, index)
-  
-    for (let i=0; i < newArr.length; i++) {
-  
-      //check that the original array contains the value
-      if (!originalArr.includes(newArr[i])) {
-        console.error(`Failed! Original array does not include ${newArr[i]}`)
+  const insertionSort = (nums) => {
+    checkLoopInvariant(nums, input, 0)
+    for (let i = 1; i < nums.length; i++) {
+      ...
+      checkLoopInvariant(nums, input, i)
+      while (j >= 0 && nums[j] > tmp) {
+        ...
       }
-  
-      //check that the new array is in sorted order
-      if (i < newArr.length - 1 && newArr[i] > newArr[i+1]) {
-        console.error(`Failed! ${newArr[i]} is not less than ${newArr[i+1]}`)
-      }
+      nums[j+1] = tmp
     }
+    checkLoopInvariant(nums, input, nums.length)
+    return nums
   }
