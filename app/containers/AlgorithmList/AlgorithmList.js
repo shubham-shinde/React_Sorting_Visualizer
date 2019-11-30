58ac9e4e76841e55a71a398b226e4b3adb145624
shivam1534 <shivam.mkp@gmail.com>
2019-11-30 22:03:37 +0530

    tooltip in buttons

--- app/containers/AlgorithmList/AlgorithmList.js
+++ app/containers/AlgorithmList/AlgorithmList.js
@@ -12,39 +12,39 @@ class AlgorithmList extends React.Component {
         <div className="buttons">
           <li >
             <button 
-              className={`offset ${sorting && (algo==='Bubble'  ? `active`: `inactive`)}`}
+              className={`offset tooltip ${sorting && (algo==='Bubble'  ? `active`: `inactive`)}`}
               onClick={() => (!sorting && this.props.actions.start_bubble_sort())} 
-            >Bubble Sort</button>
+            >Bubble Sort <span className="text"> <a href='#'> See Algorithm</a></span></button>
           </li>
           <li >
             <button 
-              className={`offset ${sorting && (algo==='Selection'  ? `active`: `inactive`)}`}
+              className={`offset tooltip ${sorting && (algo==='Selection'  ? `active`: `inactive`)}`}
               onClick={() => (!sorting && this.props.actions.selection_sort())}
-            >Selection Sort</button>
+            >Selection Sort <span className="text"> <a href='#'> See Algorithm</a></span></button>
           </li>
           <li>
             <button 
-              className={`offset ${sorting && (algo==='Quick'  ? `active`: `inactive`)}`}
+              className={`offset tooltip ${sorting && (algo==='Quick'  ? `active`: `inactive`)}`}
               onClick={() => (!sorting && this.props.actions.quick_sort())}
-            >Quick Sort</button>
+            >Quick Sort <span className="text"> <a href='#'> See Algorithm</a></span></button>
           </li>
           <li>
             <button 
-              className={`offset ${sorting && (algo==='Merge'  ? `active`: `inactive`)}`}
+              className={`offset tooltip ${sorting && (algo==='Merge'  ? `active`: `inactive`)}`}
               onClick={() => (!sorting && this.props.actions.merge_sort())} 
-            >Merge Sort</button>
+            >Merge Sort <span className="text"> <a href='#'> See Algorithm</a></span></button>
           </li>
           <li>
             <button
-              className={`offset ${sorting && (algo==='Insertion'  ? `active`: `inactive`)}`}
+              className={`offset tooltip ${sorting && (algo==='Insertion'  ? `active`: `inactive`)}`}
               onClick={() => (!sorting && this.props.actions.insertion_sort())}
-            >Insertion Sort </button>
+            >Insertion Sort <span className="text"> <a href='#'> See Algorithm</a></span></button>
           </li>
           <li>
             <button 
-              className={`offset ${sorting && (algo==='Try'  ? `active`: `inactive`)}`}
+              className={`offset tooltip ${sorting && (algo==='Try'  ? `active`: `inactive`)}`}
               onClick={() => (!sorting && this.props.actions.try_me())} 
-            >Try me</button>
+            >Try me <span className="text"> <a href='#'> What goes around comes around </a></span> </button>
           </li>
         </div>
       </div>
