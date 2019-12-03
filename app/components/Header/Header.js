7e3232c69a1ecdc04bdbf5271ed15f13a73946cd
shivam1534 <shivam.mkp@gmail.com>
2019-12-03 19:16:56 +0530

    styling of side bar

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import logo from '../../images/logo.png';
 import './Header.scss';
+import {FaExternalLinkAlt} from 'react-icons/fa'
 
 // components
 
@@ -13,18 +15,33 @@ function Header() {
   return (
     <div className="Header">
       <div  className='side-tray' alt="react-boilerplate - Logo">
-        <h1>Fuck Off</h1>
-        <ul>
-          <li>Fuck</li>
-          <li>Fuck</li>
-          <li>Fuck</li>
-          <li>Fuck</li>
-
-        </ul>
+      <div className="Head_logo">
+        <div className="logo">
+            <img src={logo}></img>
         </div>
-      <a href="https://github.com/EvilSpark/react-redux-boilerplate/">
+        <div className="heading">
+            <h1>ALGO <br></br>VISUALIZER</h1>
+        </div>
+      </div>
+        <div className='content'>
+          <li className='content-li'>
+            <div className ='open'></div>
+            <div className="tag">SORTING VISUALIZER</div>
+          </li>
+          <li className='content-li'>
+            <div className='open'></div>
+            <div className='tag'> SHORTEST PATH VISUALIZER</div>
+          </li>
+        </div>
+        <div className='portfolio'>
+            <div className='text_p'>View Portfolio</div>
+            <div className='icon_p'><FaExternalLinkAlt/></div>
+        </div>
+        <div className='help'>Help</div>
+        </div>
+      {/* <a href="https://github.com/EvilSpark/react-redux-boilerplate/">
         
-      </a>
+      </a> */}
 
       {/* <ul>
         <a href="/">Home</a>
