af65cdaaee56a1b8ef3f4c8cafd2bee52ade7b9e
kaizen97 <shinde.shubham@byjus.com>
2023-04-08 17:10:13 +0530

    correct makers update

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from '../../images/logo.png';
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import shivam from '../../images/shivam.jpg';
 import vinayak from '../../images/vinayak.png';
 import swati from '../../images/swati.png';
@@ -17,19 +17,19 @@ import './index.scss';
 import Banner from '../common/assets/images/Main.png';
 
 const MiniPortfolio = ({name, GitHubLink, ImgLink}) => (
-	<div className="portfolio">
-		<div className="photo">
-			<img alt='shubham' src={ImgLink}></img>
-		</div>
-		<div className="text_p">
-			<div>{name}</div>
-			<div className="text_p_link">
-				<a href={GitHubLink}>
-					Portfolio <FaExternalLinkAlt />
-				</a>{' '}
-			</div>
-		</div>
-	</div>
+  <div className="portfolio">
+    <div className="photo">
+      <img alt='shubham' src={ImgLink}></img>
+    </div>
+    <div className="text_p">
+      <div>{name}</div>
+      <div className="text_p_link">
+        <a href={GitHubLink}>
+          Portfolio <FaExternalLinkAlt />
+        </a>{' '}
+      </div>
+    </div>
+  </div>
 )
 
 function Header() {
@@ -75,10 +75,8 @@ function Header() {
               <FaExternalLinkAlt />
             </a>
           </div>
-			<MiniPortfolio GitHubLink="https://github.com/shubham-shinde" ImgLink={shubham} name="Shubham Shinde"/>
-			<MiniPortfolio GitHubLink="https://github.com/" ImgLink={vinayak} name="Vinayak Pateria"/>
-			<MiniPortfolio GitHubLink="https://github.com/shubham-shinde" ImgLink={swati} name="Swati Kumari"/>
-			{/* <MiniPortfolio GitHubLink="https://github.com/shivam1534" ImgLink={shivam} name="Shivam Srivastava"/> */}
+          <MiniPortfolio GitHubLink="https://github.com/shubham-shinde" ImgLink={shubham} name="Shubham Shinde" />
+          <MiniPortfolio GitHubLink="https://github.com/shivam1534" ImgLink={shivam} name="Shivam Srivastava" />
         </div>
 
         {/* <div className='help'>Help</div> */}
